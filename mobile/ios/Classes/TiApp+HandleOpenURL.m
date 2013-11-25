1711cf059ee049138ac20101fac66aec6e35f617
Paul Mietz Egli <paul@obscure.com>
2013-11-25 08:54:57 -0800

    implemented logout, showAddConnectionProvidersDialog

---
+++ mobile/ios/Classes/TiApp+HandleOpenURL.m
@@ -0,0 +1,13 @@
+//
+//  TiApp+HandleOpenURL.m
+//  gigya
+//
+//  Created by Paul Mietz Egli on 11/24/13.
+//
+//
+
+#import "TiApp+HandleOpenURL.h"
+
+@implementation TiApp (HandleOpenURL)
+
+@end
